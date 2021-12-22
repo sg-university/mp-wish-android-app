17c9f499e26813294a5ed9b63a0f00c26be713dd
bryantakari <bryantakari2018@gmail.com>
2021-12-22 22:12:22 +0700

    Test

--- app/src/main/java/com/binus/mp/wish/MainActivity.java
+++ app/src/main/java/com/binus/mp/wish/MainActivity.java
@@ -3,6 +3,7 @@ package com.binus.mp.wish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        asdasdas
+        TextView tc;
     }
 }
