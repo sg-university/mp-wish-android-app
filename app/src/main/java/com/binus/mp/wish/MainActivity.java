f4323efc9db08623ba4d45d2ceee94b64c511b25
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2021-12-22 22:31:22 +0700

    Update test

--- app/src/main/java/com/binus/mp/wish/MainActivity.java
+++ app/src/main/java/com/binus/mp/wish/MainActivity.java
@@ -1,16 +1,15 @@
 package com.binus.mp.wish;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tc;
+
+        // halo
     }
 }
